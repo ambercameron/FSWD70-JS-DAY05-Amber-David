210d89e2cb3b1b17421ea9c0325bdfc66f3b2401
Amber Cameron <ambercameron285@gmail.com>
2019-05-23 12:01:36 +0200

    update cart files

--- challenge-d/script.js
+++ challenge-d/script.js
@@ -25,3 +25,45 @@ for(let i = 0 ; i< btn.length; i++){
 	btn[i].addEventListener("click",function(){hidden(i)},false);
 }
 }
+
+
+var saleitems = {
+	"shirts": [{
+		"name": "Yeezy Shirt",
+		"price": "£80",
+		"imagesrc": "https://www.ctshirts.com/dw/image/v2/AAWJ_PRD/on/demandware.static/-/Sites-ctshirts-master/default/dw595709ff/hi-res/ACN0016WHT_a.jpg?sw=430&sh=540",
+	}, {
+		"name": "Nike Shirt",
+		"price": "£45",
+		"imagesrc": "http://www.bigwheeloffroad.com/images/rhydcountryhouse.co.uk/mens-nike-blue-white-royal-tshirt-swoosh-41LX.jpg",
+	}, {
+		"name": "New Balance Shirt",
+		"price": "£30",
+		"imagesrc": "https://nb.scene7.com/is/image/NB/mt81415bk_nb_40_i?$dw_pgp_hd$",
+	}, {
+		"name": "Puma Shirt",
+		"price": "£40",
+		"imagesrc": "https://static.keepersport.net/fileadmin/_processed_/a/b/csm_P703442-05-D1_ec9689e0f1.jpg",
+	}]
+}
+
+var i = 0
+	for (let i = 0; i < saleitems.shirts.length; i++) 
+	{ 
+		document.getElementById("visual").innerHTML += "<td><tr><td><b>Name</b></td><td>" + saleitems.shirts[i].name + 
+		"</td></tr><tr><td><b>price</b></td><td>" + saleitems.shirts[i].price + 
+		"</td></tr><tr><td><b>image</b></td><td><img src=" +  saleitems.shirts[i].imagesrc + "><button type=\"button\" class=\"cartbutton\">Add To Cart</button>"
+		}
+
+var btn = document.getElementsByClassName("cartbutton");
+ for (let i = 0; i < btn.length; i++)
+ {
+ 	btn[i].addEventListener("click",function(){addtocart(saleitems.shirts[i])},false)
+ }
+var cartarray = [];
+function addtocart(obj){
+	cartarray.push(obj);
+	console.log(cartarray)
+	
+}
+
